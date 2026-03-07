7e1c74d99c00e064e32964d84993b875bbe8089d
Kaio Nilo <kaionilofreitas@gmail.com>
2026-03-07 15:36:07 -0300

    anexando texto.txt no repositorio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            File arquivo = new File("texto.txt");
+            Scanner leitor = new Scanner(arquivo);
 
+            System.out.println("--- Iniciando Leitura do Documento ---");
+
+            while (leitor.hasNextLine()) {
+                String linha = leitor.nextLine();
+                String[] palavras = linha.split(" ");
+
+                System.out.println("Linha lida com " + palavras.length + " palavras.");
+            }
+
+            leitor.close();
+            System.out.println("--- Leitura Finalizada com Sucesso ---");
+
+        } catch (Exception e) {
+            System.err.println("Erro: Certifique-se de que 'texto.txt' está na pasta do projeto.");
+        }
     }
 }
